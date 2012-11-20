083babf4d466dc87b3a47cb365887266091f735e
Hanks <hankshz@gmail.com>
2012-11-19 19:30:17 -0800

    Add two comments for Tkinter

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ stationsInfo = [ { 'center': [ 0, 0 ], 'radius': 5, 'id': 'BS1' },
                  { 'center': [ -5, 5 ], 'radius': 5, 'id': 'BS4' },
                  { 'center': [ -5, -5 ], 'radius': 5, 'id': 'BS5' }
             ]
-handler = Tkinter.Tk()
+
+handler = Tkinter.Tk() # handler for the display
 
 clientsInfo = [ { 'id': 'Tom', 'startPosition': [ -5, -5], 'speed': [ [ 5, 5 ] ] }
 
@@ -15,4 +16,4 @@ clientsInfo = [ { 'id': 'Tom', 'startPosition': [ -5, -5], 'speed': [ [ 5, 5 ] ]
 
 celluarSystem = CelluarSystem.CelluarSystem( stationsInfo, clientsInfo, handler )
 
-Tkinter.mainloop()
+Tkinter.mainloop() # starting the display loop
