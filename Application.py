a2082ad7cccbe8e1f9746dbfe1424f67e1e74b43
Xi Chen <xx.chen@live.com>
2012-12-07 10:47:34 -0800

    Revert "Slotleft"
    This reverts commit f8c6fae827d6610ac6715d8f066aa12956433fff.

--- Application.py
+++ Application.py
@@ -7,28 +7,27 @@ class Application (object):
              self. type = type
              self. size = size
              self. TI = TI
-             self. slotleft = slotleft
-             self. max_wait_slot = round( 24 - TI / 0.125 ) # round() add by Weichao
-             if     self.type == 1:
-                      self.basic = 0.05
+             self. max_wait_slot = round( 12 - TI / 0.25 ) # round() add by Weichao
+             if      self.type == 1:
+                       self.basic = 0.05
              elif   self.type == 2:
-                      self.basic = 0.1
+                       self.basic = 0.1
              else:  
-                    self.basic = 0.15
+                       self.basic = 0.15        
        
        def getTI (self):
              return self. TI
        
-       def slot_need2 (self):
+       def slot_need2 (self, slotleft):
              slotneed = self.size / self.basic * 300
-             if slotneed <= slef.slotleft:
+             if slotneed <= slotleft:
                 return self.size / 75                # In such case it is useless to use basic bandwidth rather than full bandwidth, since cost will not change.
              else:
-                return (self.size - self.slotleft * self.basic * 300) / 75 + self.slotleft
+                return (self.size - slotleft * self.basic * 300) / 75 + slotleft
              
-       def price2 (self, curprice, newprice):
-             if self.slot_need2() > self.slotleft:
-                return self.slotleft * self.basic * 300 * curprice + (self.size - self.slotleft * self.basic * 300) * newprice
+       def price2 (self, curprice, newprice, slotleft):
+             if self.slot_need2( slotleft ) > slotleft:
+                return slotleft * self.basic * 300 * curprice + (self.size - slotleft * self.basic * 300) * newprice
              else: 
                 return curprice * self.size
 
@@ -36,32 +35,29 @@ class Application (object):
              slotneed = self.size / 75
              return self. max_wait_slot + slotneed
 
-       def price3 (self, curprice, newprice):      
-             if self.slot_need3() <= self.slotleft:
+       def price3 (self, curprice, newprice, slotleft):      
+             if self.slot_need3() <= slotleft:
                 return curprice * self.size
              else:
-                if self.max_wait_slot >= self.slotleft:
+                if self.max_wait_slot >= slotleft:
                    return newprice * self.size
                 else:
-                   return (self.slot_need3() - self.slotleft) * 75 * newprice + (self.slotleft - self.max_wait_slot) * 75 * curprice
+                   return (self.slot_need3() - slotleft) * 75 * newprice + (slotleft - self.max_wait_slot) * 75 * curprice
 
-       def next5mins (self, curprice, newprice):                                            # return the amount of data consumption in next 5 mins
-             if self.slot_need2 () - self.size / 75 > self.max_wait_slot:       # method 2 cannot be used, method 3 is the only choice
+       def next5mins (self, curprice, newprice, slotleft):                                            # return the amount of data consumption in next 5 mins
+             if self.slot_need2 (slotleft) - self.size / 75 > self.max_wait_slot:       # method 2 cannot be used, method 3 is the only choice
                 if self.max_wait_slot == 0:
                    return 75
                 else:
                    return 0
              else:                                                          # method 2 is also OK, compare the cost of both method 2 & 3, then choose the cheaper one 
-                if self.price2 (curprice, newprice) <= self. price3 (curprice, newprice):
+                if self.price2 (curprice, newprice, slotleft) <= self. price3 (curprice, newprice, slotleft):
                    return self.basic * 300
                 else:
                    if self.max_wait_slot == 0:
                       return 75
                    else:
                       return 0
-             while self.slotleft > 0:
-                   self.slotleft = self.slotleft - 1
-    
 
        def generateTraffic( self, curStation, prediction ):
             nextStation = prediction[0]
@@ -69,7 +65,7 @@ class Application (object):
 
             curprice = curStation.getPrice( 0 )
             newprice = self.predictNewPrice( curStation, nextStation, handOverSlot )
-            slotleft = handOverSlot
+            slotleft = self.calculateSlotLeft()
 
             return [self.next5mins( curprice, newprice, slotleft ), self.TI, self.type]
 
@@ -78,6 +74,10 @@ class Application (object):
             if nextStation is None:
                    return curStation.getPrice( 0 )
             return nextStation.getPrice( 0 )
+
+       def calculateSlotLeft( self ):
+            #TODO
+            return 10
                  
              
                 
